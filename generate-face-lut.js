1e8434b2434964eda50afefafcac0120c8a86120
David Arayan <david.arayan12@gmail.com>
2021-07-09 21:30:23 +1000

    - Previous LUT Generator was completely wrong, fixed and re-generated LUT tables

--- generate-face-lut.js
+++ generate-face-lut.js
@@ -29,7 +29,14 @@ const v5 = [0, 1, 1];
 const v6 = [1, 1, 1];
 const v7 = [1, 0, 1];
 
-const vertices = [v0, v1, v2, v3, v4, v5, v6, v7];
+const vertices = [];
+
+vertices[VoxelFaceGeometry.X_POS_INDEX] = [v2, v3, v6, v7];//[v3, v6, v2, v3, v7, v6];
+vertices[VoxelFaceGeometry.X_NEG_INDEX] = [v0, v1, v4, v5];//[v0, v1, v5, v0, v5, v4];
+vertices[VoxelFaceGeometry.Y_POS_INDEX] = [v1, v2, v5, v6];//[v1, v2, v5, v5, v2, v6];
+vertices[VoxelFaceGeometry.Y_NEG_INDEX] = [v0, v3, v4, v7];//[v0, v4, v3, v3, v4, v7];
+vertices[VoxelFaceGeometry.Z_POS_INDEX] = [v4, v5, v6, v7];//[v4, v5, v6, v4, v6, v7];
+vertices[VoxelFaceGeometry.Z_NEG_INDEX] = [v0, v1, v2, v3];//[v0, v2, v1, v0, v3, v2];
 
 // these are the normals for each face
 const xPosn = [1, 0, 0];
@@ -49,12 +56,12 @@ normals[VoxelFaceGeometry.Z_POS_INDEX] = zPosn;
 normals[VoxelFaceGeometry.Z_NEG_INDEX] = zNegn;
 
 // indices for each vertex, 2 triangles per face
-const xPosi = [3, 6, 2, 3, 7, 6];
-const xNegi = [0, 1, 5, 0, 5, 4];
-const yPosi = [1, 2, 5, 5, 2, 6];
-const yNegi = [0, 4, 3, 3, 4, 7];
-const zPosi = [4, 5, 6, 4, 6, 7];
-const zNegi = [0, 2, 1, 0, 3, 2];
+const xPosi = [1, 2, 0, 1, 3, 2];//[v2, v3, v6, v7]; //[3, 6, 2, 3, 7, 6];
+const xNegi = [0, 1, 3, 0, 3, 2];//[v0, v1, v4, v5]; //[0, 1, 5, 0, 5, 4];
+const yPosi = [0, 1, 2, 2, 1, 3];//[v1, v2, v5, v6]; //[1, 2, 5, 5, 2, 6];
+const yNegi = [0, 2, 1, 1, 2, 3];//[v0, v3, v4, v7]; //[0, 4, 3, 3, 4, 7];
+const zPosi = [0, 1, 2, 0, 2, 3];//[v4, v5, v6, v7]; //[4, 5, 6, 4, 6, 7];
+const zNegi = [0, 2, 1, 0, 3, 2];//[v0, v1, v2, v3]; //[0, 2, 1, 0, 3, 2];
 
 const indices = [];
 
@@ -80,21 +87,29 @@ for (let i = 0; i < totalCount; i++) {
     const worldY = coord.vy + (coord.by / bvxRenderInv);
     const worldZ = coord.vz + (coord.bz / bvxRenderInv);
 
-    for (let index = 0; index < indices.length; index++) {
-        const indx = indices[index];
-        const nrml = normals[index];
+    // for each face - cube has 6 faces
+    for (let index = 0; index < 6; index++) {
+        const nx = normals[index];
+        const px = vertices[index];
 
-        for (let px = 0; px < indx.length; px++) {
-            const vertex = vertices[indx[px]];
+        // write vertices - 4 vertices per face
+        for (let j = 0; j < px.length; j++) {
+            const vert = px[j];
 
-            verticesOut += "" + ((vertex[0] / bvxRenderInv) + worldX) + ",";
-            verticesOut += "" + ((vertex[1] / bvxRenderInv) + worldY) + ",";
-            verticesOut += "" + ((vertex[2] / bvxRenderInv) + worldZ) + ",";
+            // x pos
+            verticesOut += "" + ((vert[0] / bvxRenderInv) + worldX) + ",";
+            // y pos
+            verticesOut += "" + ((vert[1] / bvxRenderInv) + worldY) + ",";
+            // z pos
+            verticesOut += "" + ((vert[2] / bvxRenderInv) + worldZ) + ",";
 
             // write the normal corresponding to this particular face
-            normalsOut += "" + nrml[0] + ",";
-            normalsOut += "" + nrml[1] + ",";
-            normalsOut += "" + nrml[2] + ",";
+            // x pos
+            normalsOut += "" + nx[0] + ",";
+            // y pos
+            normalsOut += "" + nx[1] + ",";
+            // z pos
+            normalsOut += "" + nx[2] + ",";
         }
     }
 }
@@ -106,57 +121,69 @@ for (let i = 0; i < 64; i++) {
 
     let bvxgen = "new Int32Array([";
 
+    let offset = 0;
+
     // generate the index to render +x
     if (BitOps.bitAt(bvx, VoxelFaceGeometry.X_POS_INDEX) === 1) {
         const windex = indices[VoxelFaceGeometry.X_POS_INDEX];
 
         for (let j = 0; j < windex.length; j++) {
-            bvxgen += windex[j] + ",";
+            bvxgen += (windex[j] + offset) + ",";
         }
     }
+
+    offset += 4;
 
     // generate the index to render -x
     if (BitOps.bitAt(bvx, VoxelFaceGeometry.X_NEG_INDEX) === 1) {
         const windex = indices[VoxelFaceGeometry.X_NEG_INDEX];
 
         for (let j = 0; j < windex.length; j++) {
-            bvxgen += windex[j] + ",";
+            bvxgen += (windex[j] + offset) + ",";
         }
     }
+
+    offset += 4;
 
     // generate the index to render +y
     if (BitOps.bitAt(bvx, VoxelFaceGeometry.Y_POS_INDEX) === 1) {
         const windex = indices[VoxelFaceGeometry.Y_POS_INDEX];
 
         for (let j = 0; j < windex.length; j++) {
-            bvxgen += windex[j] + ",";
+            bvxgen += (windex[j] + offset) + ",";
         }
     }
+
+    offset += 4;
 
     // generate the index to render -y
     if (BitOps.bitAt(bvx, VoxelFaceGeometry.Y_NEG_INDEX) === 1) {
         const windex = indices[VoxelFaceGeometry.Y_NEG_INDEX];
 
         for (let j = 0; j < windex.length; j++) {
-            bvxgen += windex[j] + ",";
+            bvxgen += (windex[j] + offset) + ",";
         }
     }
+
+    offset += 4;
 
     // generate the index to render +z
     if (BitOps.bitAt(bvx, VoxelFaceGeometry.Z_POS_INDEX) === 1) {
         const windex = indices[VoxelFaceGeometry.Z_POS_INDEX];
 
         for (let j = 0; j < windex.length; j++) {
-            bvxgen += windex[j] + ",";
+            bvxgen += (windex[j] + offset) + ",";
         }
     }
+
+    offset += 4;
 
     // generate the index to render -z
     if (BitOps.bitAt(bvx, VoxelFaceGeometry.Z_NEG_INDEX) === 1) {
         const windex = indices[VoxelFaceGeometry.Z_NEG_INDEX];
 
         for (let j = 0; j < windex.length; j++) {
-            bvxgen += windex[j] + ",";
+            bvxgen += (windex[j] + offset) + ",";
         }
     }
 
