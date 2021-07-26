2c67d4433b63babdae55ca17659dad19b94b0253
David Arayan <david.arayan12@gmail.com>
2021-07-26 20:35:35 +1000

    - Removed flipped normals as they made no sense

--- generate-face-lut.js
+++ generate-face-lut.js
@@ -8,12 +8,10 @@ const generateVertices = require("./generators/generate-vertices");
 
 const VERTICES_LUT_PATH = "./src/lut/bvx-vertices.ts";
 const NORMALS_LUT_PATH = "./src/lut/bvx-normals.ts";
-const NORMALS_FLIPPED_LUT_PATH = "./src/lut/bvx-normals-flipped.ts";
 const INDICES_LUT_PATH = "./src/lut/bvx-indices.ts";
 const INDICES_FLIPPED_LUT_PATH = "./src/lut/bvx-indices-flipped.ts";
 
 generateIndices(INDICES_LUT_PATH, false);
 generateIndices(INDICES_FLIPPED_LUT_PATH, true);
 generateNormals(NORMALS_LUT_PATH, false);
-generateNormals(NORMALS_FLIPPED_LUT_PATH, true);
 generateVertices(VERTICES_LUT_PATH);
